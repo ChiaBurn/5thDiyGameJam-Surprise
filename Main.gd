e1cd95464a5db2b7e9f7b48b9f3fba510937f952
Chiahui.Chou <chiahui.chou@creativearktech.com>
2022-08-20 02:21:02 +0800

    show key selection and detect click

--- Main.gd
+++ Main.gd
@@ -6,6 +6,7 @@ export(PackedScene) var draw_scene
 export var fail_max_count: int = 5
 
 var current_quest_index: int = 0
+var current_key_code: String = "start"
 var fail_count: int = 0
 onready var position = {
 	"A": $PositionA,
@@ -30,51 +31,110 @@ const quests = [
 		"position": "B"
 	},
 	{
+		"type": "Key",
+		"life": 5000
+	},
+	{
 		"type": "Click",
 		"life": 2000,
 		"position": "C"
 	},
 	{
-		"type": "Click",
-		"life": 1000,
-		"position": "D"
+		"type": "Key",
+		"life": 5000
 	},
 	{
 		"type": "Click",
 		"life": 1000,
-		"position": "E"
-	},
-	{
-		"type": "Click",
-		"life": 1000,
-		"position": "F"
-	},
-	{
-		"type": "Click",
-		"life": 700,
 		"position": "G"
 	},
 	{
 		"type": "Click",
-		"life": 700,
+		"life": 1000,
 		"position": "H"
 	},
 	{
+		"type": "Key",
+		"life": 5000
+	},
+	{
 		"type": "Click",
-		"life": 700,
+		"life": 1000,
 		"position": "E"
 	},
 	{
 		"type": "Click",
-		"life": 500,
+		"life": 1000,
 		"position": "B"
 	},
 	{
 		"type": "Click",
-		"life": 500,
+		"life": 1000,
 		"position": "C"
 	}
 ]
+
+const keys = {
+	"start": [
+		{
+			"code": "creative", "value": "發揮創意", "position": "A"
+		},
+		{
+			"code": "passion", "value": "喜好添加", "position": "B"
+		},
+		{
+			"code": "perfect", "value": "完美還原", "position": "C"
+		}
+	],
+	"creative": [
+		{
+			"code": "creative_soul", "value": "靈魂創作", "position": "D"
+		},
+		{
+			"code": "creative_cute", "value": "可愛正義", "position": "E"
+		}
+	],
+	"creative_soul": [
+		{
+			"code": "creative_soul_meow", "value": "喵喵喵", "position": "F"
+		},
+		{
+			"code": "creative_soul_power", "value": "力與美", "position": "G"
+		}
+	],
+	"creative_cute": [
+		{
+			"code": "creative_cute_chick", "value": "可愛小雞", "position": "H"
+		},
+		{
+			"code": "creative_cute_kitty", "value": "可愛小貓", "position": "A"
+		}
+	],
+	"passion": [
+		{
+			"code": "passion_nekomimi", "value": "貓耳哈斯哈斯", "position": "E"
+		},
+		{
+			"code": "passion_furry", "value": "獸控之力", "position": "C"
+		}
+	],
+	"passion_nekomimi": [
+		{
+			"code": "passion_nekomimi_girl", "value": "貓娘哈斯哈斯", "position": "G"
+		},
+		{
+			"code": "passion_nekomimi_maid", "value": "女僕哈斯哈斯", "position": "D"
+		}
+	],
+	"passion_furry": [
+		{
+			"code": "passion_furry_shota", "value": "UWU", "position": "H"
+		},
+		{
+			"code": "passion_furry_power", "value": "POWER!", "position": "F"
+		}
+	]
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -92,6 +152,13 @@ func on_fail():
 		end_game()
 		return
 	go_next_quest()
+	
+func on_select_key(code):
+	print("select key:", code)
+	current_key_code = code
+	get_tree().call_group("keys", "queue_free")
+	go_next_quest()
+
 		
 func end_game():
 	print("Game over! current index:", current_quest_index)
@@ -102,7 +169,6 @@ func go_next_quest():
 		end_game()
 		return
 	show_current_quest()
-
 
 func show_current_quest():
 	var quest = quests[current_quest_index]	
@@ -128,8 +194,25 @@ func show_click(quest):
 
 func show_key(quest):
 	# TODO: show key
-	print("show_key:", quest)
-	pass
+	print("current_key_code:", current_key_code)
+	var current_key_selection = keys.get(current_key_code)
+	print("current_key:", current_key_selection)
+	
+	if(current_key_selection == null):
+		print("no more key object!")
+		go_next_quest()
+		return
+	
+	for selection in current_key_selection:
+		show_key_selection(selection)
+
+func show_key_selection(selection):
+	print("append selection: ", selection)
+	var key = key_scene.instance()
+	var position_node = position[selection.position]
+	key.position = position_node.position
+	key.set_code_and_value(selection.code, selection.value)
+	add_child(key);
 
 func show_event(quest):
 	# TODO: show event
